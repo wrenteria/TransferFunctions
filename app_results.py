73d79fdf43e9ac306cc3e7c8ef1f8d82023d40f8
wrenteria <wjrenteria@gmail.com>
2023-11-27 11:28:48 -0800

    App to visualize the test / tunning process for the Transfer Functions Project

---
+++ app_results.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import os
+st.set_page_config(
+    page_title="Coastal Hazard Transfer Functions  Project", page_icon=" 🌊",layout="wide" )
+
+
+st.title("Hazard Curves Model's Results")
+
+#experiments=['MLP_with_anchor_input',
+#            'MLP_without_anchor_input',
+#            'with_anchor_input',
+#            'without_anchor_input']
+       
+# path to the experiments
+path = os.path.abspath(os.path.dirname(__file__))
+folds = [name for name in os.listdir(path) if os.path.isdir(name)]
+experiments = [n for n in folds if '_input' in n]
+
+pth2exp=dict()
+for i in range(len(experiments)):
+    # path to each experiment
+    pth = os.path.join(path,experiments[i])
+    # dictionary experiment = cases
+    pth2exp[experiments[i]] = pth
+
+def floss(ruta):
+    archivos = os.listdir(ruta)
+    for f in archivos:
+        if 'oss' in f:
+            losfile = f
+    return losfile
+    
+# Using object notation
+experiment = st.sidebar.selectbox(
+    "Experiments",set(experiments))
+st.header(experiment)
+cols = st.columns(5)
+cases = os.listdir(pth2exp[experiment])
+for c in range(len(cases)):
+    ppth = os.path.join(pth2exp[experiment],cases[c])
+    iloss = floss(ppth)
+    cols[0].subheader("Case {}".format(cases[c]))
+    cols[0].image(os.path.join(ppth,iloss),use_column_width=True, caption='')
+    for i in range(4):
+        fn = 'Testing_random_Point-{}.png'.format(i)
+        ppth = os.path.join(pth2exp[experiment],cases[c])
+        cols[i+1].subheader(':gray[_]')
+        cols[i+1].image(os.path.join(ppth,fn),use_column_width=True, caption='')
